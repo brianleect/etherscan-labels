8be56f21b1db66a83d9a8a4a21d505f144e71698
brianleect <63389110+brianleect@users.noreply.github.com>
2023-03-26 19:36:59 +0800

    [Major] Generalized scrape for bsc
    Tested on single account scrape working

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ import json
 import time
 import os.path
 
+
 # Login to etherscan and auto fill login information if available
 def login():
-    driver.get('https://etherscan.io/login')
+    driver.get('{}/login'.format(baseUrl))
     driver.implicitly_wait(5)
     driver.find_element("id",
                         "ContentPlaceHolder1_txtUserName").send_keys(config['ETHERSCAN_USER'])
@@ -20,11 +21,16 @@ def login():
 
 # Retrieve label information and saves as JSON/CSV
 def getLabel(label, label_type="account", input_type='single'):
-    baseUrl = 'https://etherscan.io/{}s/label/{}?subcatid={}&size=100&start={}'
+
+    if ('eth' not in baseUrl):
+        getLabelOldFormat(label, label_type=label_type, input_type=input_type)
+        return
+
+    baseUrlLabel = baseUrl + '/{}s/label/{}?subcatid={}&size=100&start={}'
     index = 0  # Initialize start index at 0
     table_list = []
 
-    driver.get(baseUrl.format(label_type, label, 'undefined', index))
+    driver.get(baseUrlLabel.format(label_type, label, 'undefined', index))
     driver.implicitly_wait(5)
 
     # Find all elements using driver.find_elements where class matches "nav-link"
@@ -48,7 +54,7 @@ def getLabel(label, label_type="account", input_type='single'):
     for table_index, subcat_id in enumerate(subcat_id_list):
         index = 0  # Initialize start index at 0
         driver.implicitly_wait(5)
-        driver.get(baseUrl.format(label_type, label, subcat_id, index))
+        driver.get(baseUrlLabel.format(label_type, label, subcat_id, index))
         time.sleep(5)  # TODO: allow customization by args
 
         while (True):
@@ -65,10 +71,10 @@ def getLabel(label, label_type="account", input_type='single'):
                 # Retrieve all addresses from table
                 elems = driver.find_elements("xpath", "//tbody//a[@href]")
                 addressList = []
-                addrIndex = len('https://etherscan.io/address/')
+                addrIndex = len(baseUrl + '/address/')
                 for elem in elems:
                     href = elem.get_attribute("href")
-                    if (href.startswith('https://etherscan.io/address/')):
+                    if (href.startswith('baseUrl/address/')):
                         addressList.append(href[addrIndex:])
 
                 # Replace address column in newTable dataframe with addressList
@@ -100,7 +106,7 @@ def getLabel(label, label_type="account", input_type='single'):
 
     # Prints length and save as a csv
     print(label, 'Df length:', len(df.index))
-    df.to_csv('{}s/{}.csv'.format(label_type, label))
+    df.to_csv(savePath + '{}s/{}.csv'.format(label_type, label))
 
     # Save as json object with mapping address:nameTag
     if label_type == "account":
@@ -109,7 +115,7 @@ def getLabel(label, label_type="account", input_type='single'):
     if label_type == "token":
         addressNameDict = dict([(address, nameTag)
                                 for address, nameTag in zip(df.Address, df['Token Name'])])
-    with open('{}s/{}.json'.format(label_type, label), 'w', encoding='utf-8') as f:
+    with open(savePath + '{}s/{}.json'.format(label_type, label), 'w', encoding='utf-8') as f:
         json.dump(addressNameDict, f, ensure_ascii=True)
 
     if (input_type == 'single'):
@@ -120,15 +126,59 @@ def getLabel(label, label_type="account", input_type='single'):
         else:
             getLabel(endOrContinue, label_type=label_type)
 
+
+def getLabelOldFormat(label, label_type="account", input_type='single'):
+    print('Getting label old format', label)
+    baseUrlLabel = baseUrl + '/{}s/label/{}?subcatid=undefined&size=100&start={}'
+    index = 0  # Initialize start index at 0
+    table_list = []
+
+    while (True):
+        print('Index:', index)
+        driver.get(baseUrlLabel.format(label_type, label, index))
+        driver.implicitly_wait(5)
+        try:
+            newTable = pd.read_html(driver.page_source)[0]
+        except Exception as e:
+            print(e)
+            print(label, "Skipping label due to error")
+            return
+        # Remove last item which is just sum
+        table_list.append(newTable[:-1])
+        index += 100
+        if (len(newTable.index) != 101):
+            break
+    df = pd.concat(table_list)  # Combine all dataframes
+    df.fillna('', inplace=True)  # Replace NaN as empty string
+
+    # Prints length and save as a csv
+    print(label, 'Df length:', len(df.index))
+    df.to_csv(savePath + '{}s/{}.csv'.format(label_type, label))
+
+    # Save as json object with mapping address:nameTag
+    addressNameDict = dict([(address, nameTag)
+                           for address, nameTag in zip(df.Address, df['Name Tag'])])
+    with open(savePath + '{}s/{}.json'.format(label_type, label), 'w', encoding='utf-8') as f:
+        json.dump(addressNameDict, f, ensure_ascii=True)
+
+    if (input_type == 'single'):
+        endOrContinue = input(
+            'Type "exit" end to end or "label" of interest to continue')
+        if (endOrContinue == 'exit'):
+            driver.close()
+        else:
+            getLabel(endOrContinue, label_type=label_type)
+
+
 # Combines all JSON into a single file combinedLabels.json
 def combineAllJson():
     combinedAccountJSON = {}
 
     # iterating over all files
-    for files in os.listdir('./accounts'):
+    for files in os.listdir(savePath + 'accounts'):
         if files.endswith('json'):
             print(files)  # printing file name of desired extension
-            with open('./accounts/{}'.format(files)) as f:
+            with open(savePath + 'accounts/{}'.format(files)) as f:
                 dictData = json.load(f)
                 for address, nameTag in dictData.items():
                     if address not in combinedAccountJSON:
@@ -139,10 +189,10 @@ def combineAllJson():
             continue
 
     combinedTokenJSON = {}
-    for files in os.listdir('./tokens'):
+    for files in os.listdir(savePath + 'tokens'):
         if files.endswith('json'):
             print(files)  # printing file name of desired extension
-            with open('./tokens/{}'.format(files)) as f:
+            with open(savePath + 'tokens/{}'.format(files)) as f:
                 dictData = json.load(f)
                 for address, nameTag in dictData.items():
                     if address not in combinedTokenJSON:
@@ -153,50 +203,55 @@ def combineAllJson():
             continue
 
     combinedAllJSON = {
-        key: { **combinedAccountJSON.get(key, {}), **combinedTokenJSON.get(key, {}) } 
+        key: {**combinedAccountJSON.get(key, {}),
+              **combinedTokenJSON.get(key, {})}
         for key in set(list(combinedAccountJSON.keys())+list(combinedTokenJSON.keys()))
     }
 
-    with open('combined/combinedAccountLabels.json', 'w', encoding='utf-8') as f:
+    with open(savePath + 'combined/combinedAccountLabels.json', 'w', encoding='utf-8') as f:
         json.dump(combinedAccountJSON, f, ensure_ascii=True)
-    with open('combined/combinedTokenLabels.json', 'w', encoding='utf-8') as f:
+    with open(savePath + 'combined/combinedTokenLabels.json', 'w', encoding='utf-8') as f:
         json.dump(combinedTokenJSON, f, ensure_ascii=True)
-    with open('combined/combinedAllLabels.json', 'w', encoding='utf-8') as f:
+    with open(savePath + 'combined/combinedAllLabels.json', 'w', encoding='utf-8') as f:
         json.dump(combinedAllJSON, f, ensure_ascii=True)
 
 # Retrieves all labels from labelcloud and saves as JSON/CSV
+
+
 def getAllLabels():
-    driver.get('https://etherscan.io/labelcloud')
+    driver.get(baseUrl + '/labelcloud')
     driver.implicitly_wait(5)
 
     elems = driver.find_elements("xpath", "//a[@href]")
     labels = []
-    labelIndex = len('https://etherscan.io/accounts/label/')
+    labelIndex = len(baseUrl + '/accounts/label/')
     for elem in elems:
         href = elem.get_attribute("href")
-        if (href.startswith('https://etherscan.io/accounts/label/')):
+        if (href.startswith(baseUrl + '/accounts/label/')):
             labels.append(href[labelIndex:])
 
     print(labels)
     print('L:', len(labels))
 
     for label in labels:
-        if (os.path.exists('accounts/{}.json'.format(label))):
+        if (os.path.exists(savePath + 'accounts/{}.json'.format(label))):
             print(label, "'s account labels already exist, skipping.")
             continue
         elif label in ignore_list:
             print(label, 'ignored due to large size and irrelevance')
             continue
+
         getLabel(label, 'account', 'all')
         time.sleep(5)  # Give 5s interval to prevent RL
 
     for label in labels:
-        if (os.path.exists('tokens/{}.json'.format(label))):
+        if (os.path.exists(savePath + 'tokens/{}.json'.format(label))):
             print(label, "'s token labels already exist, skipping.")
             continue
         elif label in ignore_list:
             print(label, 'ignored due to large size and irrelevance')
             continue
+
         getLabel(label, 'token', 'all')
         time.sleep(5)  # Give 5s interval to prevent RL
 
@@ -207,13 +262,28 @@ def getAllLabels():
 # Large size: Eth2/gnsos , Bugged: Liqui , NoData: Remaining labels
 ignore_list = ['eth2-depositor', 'gnosis-safe-multisig', 'liqui.io', 'education', 'electronics',
                'flashbots', 'media', 'music', 'network', 'prediction-market', 'real-estate', 'vpn', 'beacon-depositor', 'uniswap']
+
 with open('config.json', 'r') as f:
     config = json.load(f)
 
+# Scan site mapping from chain to __scan url
+chainMap = {'eth': {'baseUrl': 'https://etherscan.io', 'savePath': './data/etherscan/'},
+            'bsc': {'baseUrl': 'https://bscscan.com', 'savePath': './data/bscscan/'},
+            'poly': {'baseUrl': 'https://polygonscan.com', 'savePath': './data/polygonscan/'}
+            }
 
 if __name__ == "__main__":
     driver = webdriver.Chrome(service=ChromeService(
         ChromeDriverManager().install()))
+    targetChain = input('Enter scan site of interest (eth/bsc/poly): ')
+
+    if (targetChain not in chainMap):
+        print('Invalid chain input, exiting, please try again with either eth/bsc/poly')
+        exit()
+    else:
+        baseUrl = chainMap[targetChain]['baseUrl']
+        savePath = chainMap[targetChain]['savePath']
+
     login()
     retrievalType = input('Enter retrieval type (single/all): ')
     if (retrievalType == 'all'):
